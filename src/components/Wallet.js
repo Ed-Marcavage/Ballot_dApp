9dd457b1373bbba0df4f9c8ab75eac380fb84248
Ed-Marcavage <edmarcavage256@proton.me>
2022-09-21 19:50:01 -0400

    Adds button connectivity

--- src/components/Wallet.js
+++ src/components/Wallet.js
@@ -1,15 +1,28 @@
 import React from "react";
 import { useEthers } from "@usedapp/core";
 
+const BtnStyle = {
+  borderRadius: "4px",
+  padding: "4px",
+  backgroundColor: "lightsteelblue",
+};
+
 const Wallet = () => {
   const { activateBrowserWallet, account, deactivate } = useEthers();
   return (
     <div>
       <h3>dApp Wallet</h3>{" "}
       {account ? (
-        <p>Your account: {account}</p>
+        <div>
+          <p>Your account: {account}</p>
+          <button onClick={deactivate} style={BtnStyle}>
+            Disconnect
+          </button>
+        </div>
       ) : (
-        <button onClick={activateBrowserWallet}>connect wallet</button>
+        <button onClick={() => activateBrowserWallet()} style={BtnStyle}>
+          connect wallet
+        </button>
       )}
     </div>
   );
